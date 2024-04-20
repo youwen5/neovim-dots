80cc397da46569a66a6fac6ec857542e05d531a2
Youwen Wu <youwenw@gmail.com>
2024-04-20 16:00:09 -0700

    disable copilot in markdown

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,8 +1,34 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    opts = function(_, opts)
-      table.insert(opts.filetypes, { markdown = false })
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = false,
+        help = true,
+      },
+    },
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = "copilot.lua",
+    opts = {
+      event = { "InsertEnter", "LspAttach" },
+      fix_pairs = true,
+    },
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      LazyVim.lsp.on_attach(function(client)
+        if client.name == "copilot" then
+          copilot_cmp._on_insert_enter({})
+        end
+      end)
     end,
   },
 }
