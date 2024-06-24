ee5b0e5bdbea5f8a813e84e7201060ab56be7e05
Youwen Wu <youwenw@gmail.com>
2024-06-23 19:28:23 -0700

    remove mini.animate

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+  group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
+  callback = function()
+    io.stdout:write("\x1b]1337;SetUserVar=in_editor=MQo\007")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+  group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
+  callback = function()
+    io.stdout:write("\x1b]1337;SetUserVar=in_editor\007")
+  end,
+})
