e0fbcc0c8f203dfcb608d53674bee0f39f399bc1
Youwen Wu <youwenw@gmail.com>
2024-08-12 20:14:12 -0700

    feat: add kitty integration

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -11,4 +11,16 @@ return {
     },
   },
   { 'wakatime/vim-wakatime', lazy = false },
+  {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = true,
+    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+    event = { 'User KittyScrollbackLaunch' },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^5.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require('kitty-scrollback').setup()
+    end,
+  },
 }
